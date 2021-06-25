83aa782b65b3685583879562f55f9bc8f76f2055
renato roessler <renato@gmail.com.br>
2021-06-25 16:53:28 -0300

    falta

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid Two Columns',
+  text: `Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.`,
+  srcImg: 'assets/images/javascript.svg',
+};
